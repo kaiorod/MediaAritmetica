8f68c7fec682a0c5bb39066aa5457cfa01c27f5a
kaiorod <caiorodrigo2775@gmail.com>
2021-09-12 23:34:02 -0300

    Finaliza MediaAritmetica

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, media;
+
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite o 1º número : ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o 2º número : ");
+            n2 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o 3º número : ");
+            n3 = double.Parse(Console.ReadLine());
+
+            media = (n1 + n2 + n3) / 3;
+
+            Console.Write($"\nMédia : ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(media);
+            Console.ResetColor();
+        }
+    }
+}
